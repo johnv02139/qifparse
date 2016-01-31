84f4169a15d98d0d2e42f93cbbf46c0cf31fc824
John Valente <johnv02139@yahoo.com>
2016-01-30 16:39:09 -0800

    Improve commenting.

--- qifparse/tests/test_parse.py
+++ qifparse/tests/test_parse.py
@@ -11,14 +11,35 @@ def stripAllLines(txt):
 
 class TestQIFParsing(unittest.TestCase):
 
+    # This test simply reads in the file and asserts that an object is created
+    # from it, with no exceptions.
     def testParseFile(self):
         qif = QifParser.parse(open(filename, 'U'), '%d/%m/%Y')
         self.assertTrue(qif)
 
+    # The "testWrite" tests verify that we can read text from a QIF file, and
+    # then recreate it, virtually identically, from the internal data structures
+    # created.  If we're able to do that, we can feel pretty confident that we
+    # captured all the relevant data from the file.  At the same time, being
+    # able to recreate a QIF file exactly is neither necessary nor sufficient
+    # to saying that we've actually understood what the data means.
+    #
+    # I say "virtually identical" because we use the stripAllLines function,
+    # defined above, to create a version of the input that has no superfluous
+    # whitespace at the end of lines.  (The alternative would be to make the
+    # qif class retain the whitespace, which would be silly.)  Also, by virtue
+    # of the 'U' flag given to open, we are able to ignore newlines, and
+    # effectively create a version of the file with Unix-style newlines (by
+    # rejoining the individual lines with '\n'), which is what we compare with
+    # our output.
     def testWriteFile(self):
         data = open(filename, 'U').read()
         qif = QifParser.parse(open(filename, 'U'), '%d/%m/%Y')
         stripped = stripAllLines(data)
+# If the strings are not equal, it could be useful to use the "diff" tool from
+# the shell to compare them.  The lines below would write out the parsed data to
+# a file, which could then be diffed.  But it's not necessary or useful when
+# running in an automated way.
 #        out = open('out.qif', 'w')
 #        out.write(str(qif))
 #        out.close()
@@ -28,9 +49,6 @@ class TestQIFParsing(unittest.TestCase):
         data = open(filename2, 'U').read()
         qif = QifParser.parse(open(filename2, 'U'), '%d/%m/%Y')
         stripped = stripAllLines(data)
-#        out = open('out.qif', 'w')
-#        out.write(str(qif))
-#        out.close()
         self.assertEquals(stripped, str(qif))
 
 if __name__ == "__main__":
